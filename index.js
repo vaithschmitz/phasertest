fa49cfaf4b26554bee5be52384748cd3d3008f23
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-12 15:13:26 +0100

    player and world

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -9,7 +16,7 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload ()
 {
@@ -23,9 +30,46 @@ function preload ()
 function create ()
 {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+    platforms = this.physics.add.staticGroup();
+
+    // setScale scales img by x, call refresh body for changes to take effect
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');  
+    
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+
+
 }
 
 function update ()
 {
 }
+
+
